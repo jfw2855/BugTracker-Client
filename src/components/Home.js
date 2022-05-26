b3362bd80fdfdcb5b3be914145290f96fa6472aa
jfw2855 <jwaddle1130@gmail.com>
2022-05-26 07:59:00 -0500

    adds projectsIndex component to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
+import ProjectsIndex from "./projects/ProjectsIndex"
+
 const Home = (props) => {
-	// const { msgAlert, user } = props
-	console.log('props in home', props)
+	const { msgAlert, user } = props
 
 	return (
 		<>
 			<h2>Home Page</h2>
+			<ProjectsIndex user={user}/>
 		</>
 	)
 }
