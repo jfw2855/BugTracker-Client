08d2467cdad07046c25d8762c402577adf0d9f9a
jfw2855 <jwaddle1130@gmail.com>
2022-05-26 08:01:18 -0500

    adds api call to get project data

--- src/api/project.js
+++ src/api/project.js
@@ -2,9 +2,22 @@ import apiUrl from '../apiConfig'
 import axios from 'axios'
 
 
-// create project
+// POST -> creates project
 
-// index projects
+
+
+// GET -> index projects
+export const showProjects = (user) => {
+
+    const config = {
+      method: 'GET',
+      url: `${apiUrl}/project`,
+      headers: {
+          Authorization: `Token token=${user.token}`
+      }
+    }
+    return axios(config)
+  }
 
 // show project
 
