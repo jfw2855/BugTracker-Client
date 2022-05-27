c4a644ea808c928e996aafa5329092f093aef505
jfw2855 <jwaddle1130@gmail.com>
2022-05-27 18:41:47 -0500

    adds project and issues components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import ProjectsIndex from "./projects/ProjectsIndex"
+import IssuesIndex from "./issues/IssuesIndex"
 import { ListGroup } from "react-bootstrap"
 
 const Home = (props) => {
@@ -7,9 +8,12 @@ const Home = (props) => {
 	return (
 		<>
 			<h2>Home Page</h2>
-			<div style={{border:'1px solid red'}}>
+			<div style={{border:'1px solid red',display:'flex',justifyContent:'space-between'}}>
                 <ListGroup style={{border:'2px dashed purple',width:'30%'}}>
 					<ProjectsIndex user={user}/>
+                </ListGroup>
+				<ListGroup style={{border:'2px dashed purple',width:'30%'}}>
+					<IssuesIndex user={user}/>
                 </ListGroup>
 			</div>
 		</>
