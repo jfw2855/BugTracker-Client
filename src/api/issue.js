41e5dd2935bf1ed97454720e11db8af795142d61
jfw2855 <jwaddle1130@gmail.com>
2022-05-27 15:56:00 -0500

    adds get request function for user's issue data

--- src/api/issue.js
+++ src/api/issue.js
@@ -7,6 +7,19 @@ import axios from 'axios'
 
 //show issues
 
+//GET user issues -> displays all issues user created 
+export const showMyIssues = (user) => {
+
+    const config = {
+      method: 'GET',
+      url: `${apiUrl}/user/issues`,
+      headers: {
+          Authorization: `Token token=${user.token}`
+      }
+    }
+    return axios(config)
+  }
+
 // show individual issue
 
 // update issue
