1d4fc77f3b293f47750856fb062267d28e4cc9e4
jfw2855 <jwaddle1130@gmail.com>
2022-05-27 15:17:56 -0500

    initial commit

---
+++ src/components/projects/issues/IssuesIndex.js
@@ -0,0 +1,17 @@
+import { showProjects } from "../../api/project"
+import { useEffect,useState } from "react"
+import { Spinner, ListGroup, Row, Col } from 'react-bootstrap'
+
+
+
+const IssuesIndex = (props) => {
+    const {user} = props
+    
+
+	return (
+		<> 
+		</>
+	)
+}
+
+export default IssuesIndex
