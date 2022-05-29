ca865a2b1a99e40a749a43dd56f57a285260d6a2
jfw2855 <jwaddle1130@gmail.com>
2022-05-29 17:30:48 -0500

    adds a refresh function to render updated projects index

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import ProjectsIndex from "./projects/ProjectsIndex"
 import IssuesIndex from "./issues/IssuesIndex"
 import { Button, ButtonGroup, ListGroup } from "react-bootstrap"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ProjectNewModal from "./projects/ProjectNewModal"
 import { createProject } from "../api/project"
 
@@ -9,6 +9,13 @@ const Home = (props) => {
 	const { msgAlert, user } = props
 	const [projectOpen,setProjectOpen] = useState(false)
 	const [issueOpen,setIssueOpen] = useState(false)
+	const [projectRefresh,setProjectRefresh] = useState(false)
+
+	//resets the refresh state variable
+	useEffect(() => {
+		setProjectRefresh(false)
+
+	},[projectRefresh])
 	
 	//handle function to open New Project Modal
 	const handleNewProject = () => {
@@ -20,12 +27,17 @@ const Home = (props) => {
 		setIssueOpen(true)
 	}
 
+	//rerenders project index once a new project is created
+	const refreshProjects = () => {
+		setProjectRefresh(true)
+	}
+
 	return (
 		<>
 			<h2>Home Page</h2>
 			<div style={{border:'1px solid red',display:'flex',justifyContent:'space-between'}}>
                 <ListGroup style={{border:'2px dashed purple',width:'30%'}}>
-					<ProjectsIndex user={user}/>
+					<ProjectsIndex user={user} refresh={projectRefresh}/>
                 </ListGroup>
 				<ListGroup style={{border:'2px dashed purple',width:'30%'}}>
 					<IssuesIndex user={user}/>
@@ -44,6 +56,7 @@ const Home = (props) => {
 				user={user}
 				msgAlert={msgAlert}
 				createProject={createProject}
+				refreshProjects={refreshProjects}
 				handleClose={() => {
 					setProjectOpen(false)
 				}}
