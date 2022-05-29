98f14ef02c6733d7966ae331d66f212cc6f621f7
jfw2855 <jwaddle1130@gmail.com>
2022-05-29 17:40:24 -0500

    initial commit

---
+++ src/components/issues/IssueNewModal.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import {Modal, Container, Form, Button, Col, Row } from 'react-bootstrap'
+
+
+const IssueNewModal = (props) => {
+
+
+
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default IssueNewModal
