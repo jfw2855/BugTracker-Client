72a70c27a58ac5a66e2a83bdba16895466e07774
jfw2855 <jwaddle1130@gmail.com>
2022-05-29 13:51:30 -0500

    adds form to create project modal

--- src/components/projects/ProjectNewModal.js
+++ src/components/projects/ProjectNewModal.js
@@ -4,9 +4,61 @@ import {Modal, Container, Form, Button, Col, Row } from 'react-bootstrap'
 
 const ProjectNewModal = (props) => {
 
+    const { show, user, triggerRefresh, createProject, handleClose, msgAlert } = props
+    const [project, setProject] = useState('')
+
+    // updates the project state variable for each key stroke
+    const handleChange = (e) => {
+        // e === event
+        e.persist()
+
+        setProject((prevTrans) => {
+        const name = e.target.name
+        let value = e.target.value
+
+        const updatedValue = { [name]: value }
+
+        return { ...prevTrans, ...updatedValue }
+        })
+    }
+
 
   return (
     <>
+    <Modal show={show} onHide={handleClose} style={{ color: 'black' }}>
+      <Modal.Header closeButton style={{ color: 'black' }}>
+        Create Project
+      </Modal.Header>
+      <Modal.Body>
+        <Container className="justify-content-center">
+          <Form>
+            <Row>
+              <Col>
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  name="title"
+                  type="string"
+                  onChange={handleChange}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  name="description"
+                  type="string"
+                  onChange={handleChange}
+                />
+              </Col>
+            </Row>
+            <Button className="new-transaction-btn" type="submit">
+              Add Project
+            </Button>
+          </Form>
+        </Container>
+      </Modal.Body>
+    </Modal>
     </>
   )
 }
