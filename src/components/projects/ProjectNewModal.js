2438a1479d9744582065d7e914d498d64071c44a
jfw2855 <jwaddle1130@gmail.com>
2022-05-29 13:23:42 -0500

    initial commit

---
+++ src/components/projects/ProjectNewModal.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import {Modal, Container, Form, Button, Col, Row } from 'react-bootstrap'
+
+
+const ProjectNewModal = (props) => {
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default ProjectNewModal
