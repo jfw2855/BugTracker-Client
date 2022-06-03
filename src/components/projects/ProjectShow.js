da0bf210a5396ae6d982f26026a888bc10f2d31e
jfw2855 <jwaddle1130@gmail.com>
2022-06-03 16:10:53 -0500

    adds html elements to display project details

--- src/components/projects/ProjectShow.js
+++ src/components/projects/ProjectShow.js
@@ -1,13 +1,46 @@
-
+import { useLocation,useParams } from "react-router-dom"
+import { Button,ListGroup,ListGroupItem,Row,Col } from "react-bootstrap"
+import { useEffect,useState } from "react"
+import { showProjectIssues } from "../../api/issue"
 
 
 
 const ProjectShow = (props) => {
 
+    const {user} = props
+    const location = useLocation()
+    const {title,description,owner} = location.state
+
+
+
+
+
 
 	return (
 		<>
-        <h1>Project Show page</h1>
+            <h1>{`Project: ${title}`}</h1>
+            <Button variant='warning'>
+                Edit
+            </Button>
+            <Button variant='danger'>
+                Remove Project
+            </Button>
+            <p>
+                {description}
+            </p>
+            <div>
+                <ListGroup>
+                    <ListGroupItem>
+                        <Row>
+                            <Col>Status</Col>
+                            <Col>Priority</Col>
+                            <Col>Title</Col>
+                        </Row>
+
+                    </ListGroupItem>
+                </ListGroup>
+            </div>
+            
 		</>
 	)
 }
