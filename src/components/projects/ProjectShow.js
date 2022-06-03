768afb19c5407552d425f6d410630af8d7e84879
jfw2855 <jwaddle1130@gmail.com>
2022-06-03 16:58:46 -0500

    renders issue details with color specs for priority type and status

--- src/components/projects/ProjectShow.js
+++ src/components/projects/ProjectShow.js
@@ -1,5 +1,5 @@
 import { useLocation,useParams } from "react-router-dom"
-import { Button,ListGroup,ListGroupItem,Row,Col } from "react-bootstrap"
+import { Button,ListGroup,ListGroupItem,Row,Col, Spinner } from "react-bootstrap"
 import { useEffect,useState } from "react"
 import { showProjectIssues } from "../../api/issue"
 
@@ -11,21 +11,61 @@ const ProjectShow = (props) => {
     const location = useLocation()
     const {title,description,owner} = location.state
     const params = useParams()
-    const {projId} = params 
+    const {projId} = params
+    const [issues,setIssues]=useState(null)
+
+    let issueDetails
 
 
     useEffect(()=>{
         fetchIssues()
-
-        
-    })
+    },[])
 
     //function used in useEffect to fetch all project issues from db
     const fetchIssues =  async () => {
         let apiResp = await showProjectIssues(user,projId)
-        console.log('this is issues from apiResp!!!!',apiResp.data.issues)
+        setIssues(apiResp.data.issues)
+        console.log('this is issues from apiResp!!!!',apiResp.data.issues)   
+    }
+
+    //shows spinner while awaiting for api resp
+    if (!issues) {
+        return<Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading</span>
+        </Spinner>
+    } else {
+        issueDetails = issues.map((issue,index)=>{
+            return(
+            <ListGroupItem key={`issueKey${issue._id}`}>
+                <Row>
+                    <Col
+                    className="issue-details"
+                    style={{backgroundColor: issue.status==='open'?'lightgreen':'lightgrey'}}
+                    >
+                        {issue.status}
+                    </Col>
+                    <Col
+                    className="issue-details"
+                    style = {{
+                        'backgroundColor': issue.priority === 'low' ?
+                        'lightyellow' : issue.priority === 'medium' ?
+                        'yellow' : issue.priority === 'high'? 'orange':'red'
+                    }}
+                    >
+                        {issue.priority}
+                    </Col>
+                    <Col className="issue-details">{issue.title}</Col>
+                    <Col className="issue-details">{issue.comments.length}</Col>
+                </Row>
+            </ListGroupItem>
+            )
+
+        })
         
     }
+
+
+    
 
 
 	return (
@@ -44,12 +84,13 @@ const ProjectShow = (props) => {
                 <ListGroup>
                     <ListGroupItem>
                         <Row>
-                            <Col>Status</Col>
-                            <Col>Priority</Col>
-                            <Col>Title</Col>
+                            <Col className="issue-details">Status</Col>
+                            <Col className="issue-details">Priority</Col>
+                            <Col className="issue-details">Issue</Col>
+                            <Col className="issue-details">Comments</Col>
                         </Row>
-
                     </ListGroupItem>
+                    {issueDetails}
                 </ListGroup>
             </div>
             
