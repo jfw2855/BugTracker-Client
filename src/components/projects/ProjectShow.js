26c90c62ef2f83bac28e56ce53c8b3922245ea43
jfw2855 <jwaddle1130@gmail.com>
2022-06-04 17:30:33 -0500

    saves priority and status data to objects for graphs

--- src/components/projects/ProjectShow.js
+++ src/components/projects/ProjectShow.js
@@ -15,17 +15,42 @@ const ProjectShow = (props) => {
     const [issues,setIssues]=useState(null)
 
     let issueDetails
+    let priorityData = {}
+    let statusData = {}
+
 
 
     useEffect(()=>{
         fetchIssues()
     },[])
-
+    
     //function used in useEffect to fetch all project issues from db
     const fetchIssues =  async () => {
         let apiResp = await showProjectIssues(user,projId)
         setIssues(apiResp.data.issues)
-        console.log('this is issues from apiResp!!!!',apiResp.data.issues)   
+        console.log('this is issues from apiResp!!!!',apiResp.data.issues)
+        createGData(apiResp.data.issues)  
+    }
+    
+    //function that loops through issues to create issue graphs
+    const createGData = (issues) => {
+        for (let i in issues) {
+            let priority = issues[i].priority
+            let status = issues[i].status
+            //checks if priority & status exists in objects
+            if (priority in priorityData) {
+                priorityData[`${priority}`]+=1
+            }
+            else {
+                priorityData[`${priority}`] = 1
+            }
+            if (status in statusData) {
+                statusData[`${status}`]+=1
+            }
+            else {
+                statusData[`${status}`] = 1
+            }
+        }
     }
 
     //shows spinner while awaiting for api resp
@@ -33,6 +58,8 @@ const ProjectShow = (props) => {
         return<Spinner animation="border" role="status">
         <span className="visually-hidden">Loading</span>
         </Spinner>
+        
+    //maps issues to create list group items of issues
     } else {
         issueDetails = issues.map((issue,index)=>{
             return(
@@ -65,31 +92,34 @@ const ProjectShow = (props) => {
     }
 
 
-    
-
 
 	return (
 		<>
-            <h1>{`Project: ${title}`}</h1>
-            <Button variant='warning'>
-                Edit
-            </Button>
-            <Button variant='danger'>
-                Remove Project
-            </Button>
-            <p>
-                {description}
-            </p>
-            <div>
-                <ListGroup>
-                    <ListGroupItem>
-                        <Row>
-                            <Col className="issue-details">Status</Col>
-                            <Col className="issue-details">Priority</Col>
-                            <Col className="issue-details">Issue</Col>
-                            <Col className="issue-details">Comments</Col>
-                        </Row>
-                    </ListGroupItem>
+            <div className="project-header">
+                <h1>{`Project: ${title}`}</h1>
+                <Button variant='warning'>
+                    Edit
+                </Button>
+                <Button variant='danger'>
+                    Remove Project
+                </Button>
+
+            </div>
+            <div className="project-body">
+                <p>
+                    {description}
+                </p>
+            </div>
+            <div className="project-footer">
+                <ListGroupItem style={{width:'70%'}}>
+                    <Row>
+                        <Col className="issue-details">Status</Col>
+                        <Col className="issue-details">Priority</Col>
+                        <Col className="issue-details">Issue</Col>
+                        <Col className="issue-details">Comments</Col>
+                    </Row>
+                </ListGroupItem>
+                <ListGroup className="issues-listgroup">
                     {issueDetails}
                 </ListGroup>
             </div>
