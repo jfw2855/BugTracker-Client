2eb6de24eec2cf1351947198386909075486166f
jfw2855 <jwaddle1130@gmail.com>
2022-06-11 14:16:25 -0500

    adds comment post route

--- src/api/comment.js
+++ src/api/comment.js
@@ -2,6 +2,20 @@ import apiUrl from '../apiConfig'
 import axios from 'axios'
 
 
-//POST
+//POST -> create comment
+export const createComment = (user,issueId,comment) => {
+
+    const config = {
+      method: 'POST',
+      url: `${apiUrl}/comment/issue/${issueId}`,
+      headers: {
+        Authorization: `Token token=${user.token}`
+      },
+      data: {comment}
+    }
+    return axios(config)
+  }
+
+
 //PATCH
 //DELETE
