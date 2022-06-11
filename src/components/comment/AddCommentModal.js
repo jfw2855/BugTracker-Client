f80179b624d198df72bf4c22ee75f403504ff378
jfw2855 <jwaddle1130@gmail.com>
2022-06-11 14:16:57 -0500

    saves comment to db and closes modal

--- src/components/comment/AddCommentModal.js
+++ src/components/comment/AddCommentModal.js
@@ -4,7 +4,7 @@ import { createComment } from '../../api/comment'
 
 
 const AddCommentModal = (props) => {
-    const {show, user, issueId, refresh, handleClose} = props
+    const {show, user, issueId, refreshComments, handleClose} = props
     const [comment,setComment] = useState('')
 
     const handleChange = (e) => {
@@ -21,6 +21,14 @@ const AddCommentModal = (props) => {
         })
     }
 
+    //creates new comment 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await createComment(user,issueId,comment)
+        refreshComments()
+        handleClose()
+    }
+
 	return (
         <>
         <Modal show={show} onHide={handleClose} style={{ color: 'black' }}>
@@ -28,11 +36,17 @@ const AddCommentModal = (props) => {
                 Add Comment
             </Modal.Header>
             <Modal.Body>
+                <Form onSubmit={handleSubmit}>
                     <Form.Control
-                    name="description"
+                    name="body"
                     type="string"
                     onChange={handleChange}
+                    style={{height:'200px'}}
                     />
+                <Button className="new-issue-btn" type="submit">
+                Add Comment
+                </Button>
+                </Form>
             </Modal.Body>
         </Modal>
         </>
