0f8036c6efdb5374238228da7c051ad9c15369b1
jfw2855 <jwaddle1130@gmail.com>
2022-06-20 18:53:27 -0500

    renders issue and project stats

--- src/components/shared/StatData.js
+++ src/components/shared/StatData.js
@@ -1,12 +1,22 @@
 
-
-
 const StatData = (props) => {
 
+    const {issuesData,projects,user} = props
 
+    let [open,closed] = Array(2).fill(0)
+
+    for (let i in issuesData) {
+        issuesData[i].status=="open"?open+=1:closed+=1
+    }
+
+    console.log('from statdata',issuesData,projects)
 
 	return (
-		<> 
+		<>  
+            <h3>Total {user.organization.toUpperCase()} Projects: {projects.length}</h3>
+            <h3>Open Issues: {open}</h3>
+            <h3>Closed Issues: {closed}</h3>
+        
 
 		</>
 	)
