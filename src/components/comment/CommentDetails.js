4185ad9aea1ee000cfb18e8cf3618bf3f566ae0d
jfw2855 <jwaddle1130@gmail.com>
2022-06-21 11:29:18 -0500

    fixes error issue

--- src/components/comment/CommentDetails.js
+++ src/components/comment/CommentDetails.js
@@ -38,9 +38,9 @@ const CommentDetails = (props) => {
                     <Col>
                         {comment.owner.firstName} {comment.owner.lastName}
                         , {
-                            comment.owner.role=="be"?"Backend Engineer":comment.owner.role=="ds"?
-                            "Data Scientist":comment.owner.role=="fe"?"Frontend Engineer":comment.owner.role=="intern"?
-                            "Intern":comment.owner.role=="pm"?"Project Manger":comment.owner.role=="tl"?"Tech Lead":"Fullstack Engineer"
+                            comment.owner.role=="be"?"Backend Engineer":comment.owner.role==="ds"?
+                            "Data Scientist":comment.owner.role==="fe"?"Frontend Engineer":comment.owner.role==="intern"?
+                            "Intern":comment.owner.role==="pm"?"Project Manger":comment.owner.role==="tl"?"Tech Lead":"Fullstack Engineer"
                         }
                     </Col>
                     <Col>{comment.createdAt===comment.updatedAt?
