e0cedef0f4fd8f5d2e05bbe6166952e1884b1f37
jfw2855 <jwaddle1130@gmail.com>
2022-07-01 17:02:07 -0500

    removes unused dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-// import React, { Component, Fragment } from 'react'
 import React, { useState, Fragment } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { v4 as uuid } from 'uuid'
-
-// import AuthenticatedRoute from './components/shared/AuthenticatedRoute'
 import AutoDismissAlert from './components/shared/AutoDismissAlert/AutoDismissAlert'
 import Header from './components/shared/Header'
 import RequireAuth from './components/shared/RequireAuth'
