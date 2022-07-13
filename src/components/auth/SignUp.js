b12aa6ba8f1f4d4311ae5ee0619941b8195289d0
jfw2855 <jwaddle1130@gmail.com>
2022-07-13 13:32:08 -0500

    adds classes and row and col section for name

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,12 +1,11 @@
 // import React, { Component } from 'react'
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate,Link } from 'react-router-dom'
 
 import { signUp, signIn } from '../../api/auth'
 import messages from '../shared/AutoDismissAlert/messages'
+import {Form,Button,Row,Col} from 'react-bootstrap'
 
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 
 const SignUp = (props) => {
   
@@ -53,34 +52,44 @@ const SignUp = (props) => {
 
 
     return (
-        <div className='row'>
-            <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                <h3>Sign Up</h3>
-                <Form onSubmit={onSignUp}>
-                    <Form.Group controlId='firstName'>
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control
-                                required
-                                type='string'
-                                name='firstName'
-                                value={first}
-                                placeholder='Enter First Name'
-                                onChange={e => setFirst(e.target.value)}
-                            />
-                        </Form.Group>
-                    <Form.Group controlId='lastName'>
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control
-                            required
-                            type='string'
-                            name='lastName'
-                            value={last}
-                            placeholder='Enter Last Name'
-                            onChange={e => setLast(e.target.value)}
-                        />
-                    </Form.Group>
+        <div className='credentials-container'>
+            <div className='signup'>
+                <h3 className='auth-header'>Sign Up</h3>
+                <p className='new-user'>
+                    Have An Account?&nbsp; 
+                    <Link to='/sign-in'>Sign In</Link>
+                </p>
+                <Form onSubmit={onSignUp} className="signin-form">
+                    <Row>
+                        <Col>
+                            <Form.Group controlId='firstName' className='form-group'>
+                                    <Form.Control
+                                        required
+                                        type='string'
+                                        name='firstName'
+                                        value={first}
+                                        placeholder='Enter First Name'
+                                        onChange={e => setFirst(e.target.value)}
+                                    />
+                                </Form.Group>
+
+                        </Col>
+                        <Col>
+                            <Form.Group controlId='lastName' className='form-group'>
+                                <Form.Control
+                                    required
+                                    type='string'
+                                    name='lastName'
+                                    value={last}
+                                    placeholder='Enter Last Name'
+                                    onChange={e => setLast(e.target.value)}
+                                />
+                            </Form.Group>
+
+                        </Col>
+                    </Row>
                     
-                    <Form.Group controlId='email'>
+                    <Form.Group controlId='email' className='form-group'>
                         <Form.Label>Email address</Form.Label>
                         <Form.Control
                             required
@@ -91,37 +100,43 @@ const SignUp = (props) => {
                             onChange={e => setEmail(e.target.value)}
                         />
                     </Form.Group>
-                    <Form.Group controlId="formBasicSelect">
-                        <Form.Label>Role</Form.Label>
-                        <Form.Control
-                            as="select"
-                            onChange={e => setRole(e.target.value)}
-                            name="type"
-                            type="string"
-                        >
-                            <option value="">Select Role</option>
-                            <option value="be">Back End Engineer</option>
-                            <option value="ds">Data Scientist</option>
-                            <option value="fe">Front End Engineer</option>
-                            <option value="fs">Full Stack Engineer</option>
-                            <option value="intern">Intern</option>
-                            <option value="pm">Product Manager</option>
-                            <option value="tl">Tech Lead</option>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicSelect">
-                        <Form.Label>Company/Organization</Form.Label>
-                        <Form.Control
-                            as="select"
-                            onChange={e => setOrg(e.target.value)}
-                            name="type"
-                            type="string"
-                        >
-                            <option value="">Select Company/Organization</option>
-                            <option value="demo">Demo</option>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId='password'>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="formBasicSelect" className='form-group'>
+                                <Form.Label>Role</Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    onChange={e => setRole(e.target.value)}
+                                    name="type"
+                                    type="string"
+                                >
+                                    <option value="">Select Role</option>
+                                    <option value="be">Back End Engineer</option>
+                                    <option value="ds">Data Scientist</option>
+                                    <option value="fe">Front End Engineer</option>
+                                    <option value="fs">Full Stack Engineer</option>
+                                    <option value="intern">Intern</option>
+                                    <option value="pm">Product Manager</option>
+                                    <option value="tl">Tech Lead</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="formBasicSelect" className='form-group'>
+                                <Form.Label>Company/Organization</Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    onChange={e => setOrg(e.target.value)}
+                                    name="type"
+                                    type="string"
+                                >
+                                    <option value="">Select Company/Organization</option>
+                                    <option value="demo">Demo</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Form.Group controlId='password' className='form-group'>
                         <Form.Label>Password</Form.Label>
                         <Form.Control
                             required
@@ -132,7 +147,7 @@ const SignUp = (props) => {
                             onChange={e => setPassword(e.target.value)}
                         />
                     </Form.Group>
-                    <Form.Group controlId='passwordConfirmation'>
+                    <Form.Group controlId='passwordConfirmation' className='form-group'>
                         <Form.Label>Password Confirmation</Form.Label>
                         <Form.Control
                             required
@@ -143,9 +158,9 @@ const SignUp = (props) => {
                             onChange={e => setPasswordConfirmation(e.target.value)}
                         />
                     </Form.Group>
-                    <Button variant='primary' type='submit'>
-                        Submit
-                    </Button>
+                    <button className='auth-button' type='submit'>
+                        Sign Up
+                    </button>
                 </Form>
             </div>
         </div>
