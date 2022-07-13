90acc958fb5c5d8092c0713fb97a415cd4d86315
jfw2855 <jwaddle1130@gmail.com>
2022-07-13 13:51:14 -0500

    navigates to sign in page after user logs out

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,37 +1,53 @@
 import React, { Fragment } from 'react'
 import {Nav,NavDropdown,Navbar} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import {BiBug} from 'react-icons/bi'
+import { signOut } from '../../api/auth'
+import messages from './AutoDismissAlert/messages'
 
 const linkStyle = {
     color: 'white',
     textDecoration: 'none'
 }
-const authenticatedOptions = (
-	<NavDropdown title="My Account&nbsp;&nbsp;&nbsp;"  id="dropdown-style">
-		<NavDropdown.Item className="navv-link">
-			<Link to='change-password' className="navv-link">Change Password</Link>
-		</NavDropdown.Item>
-		<NavDropdown.Item className="navv-link" >
-			<Link to='sign-out' className="navv-link">Sign Out</Link>
-		</NavDropdown.Item>
-	</NavDropdown>
-)
 
-const unauthenticatedOptions = (
-	<>
-        <Nav.Link>
-		    <Link to='sign-up' style={linkStyle}>Sign Up</Link>
-        </Nav.Link>
-        <Nav.Link>
-		    <Link to='sign-in' style={linkStyle}>Sign In</Link>
-        </Nav.Link>
-	</>
-)
+const Header = ({ user,clearUser,msgAlert }) => {
+	
+	const navigate = useNavigate()
+    const onSignOut = () => {
+		signOut(user)
+			.finally(() =>
+				msgAlert({
+					heading: 'Signed Out Successfully',
+					message: messages.signOutSuccess,
+					variant: 'success',
+				})
+			)
+			.finally(() => navigate('/sign-in'))
+			.finally(() => clearUser())
+    }
+	const authenticatedOptions = (
+		<NavDropdown title="My Account&nbsp;&nbsp;&nbsp;"  id="dropdown-style">
+			<NavDropdown.Item className="navv-link">
+				<Link to='change-password' className="navv-link">Change Password</Link>
+			</NavDropdown.Item>
+			<NavDropdown.Item className="navv-link" >
+				<div onClick={onSignOut} className="navv-link">Sign Out</div>
+			</NavDropdown.Item>
+		</NavDropdown>
+	)
+	
+	const unauthenticatedOptions = (
+		<>
+			<Nav.Link>
+				<Link to='sign-up' style={linkStyle}>Sign Up</Link>
+			</Nav.Link>
+			<Nav.Link>
+				<Link to='sign-in' style={linkStyle}>Sign In</Link>
+			</Nav.Link>
+		</>
+	)
 
-
-
-const Header = ({ user }) => (
+	return (
 	<Navbar bg='dark' >
 		<Navbar.Brand className='icon-nav'>
 			{
@@ -53,6 +69,6 @@ const Header = ({ user }) => (
 			</Nav>
 		</Navbar.Collapse>
 	</Navbar>
-)
+)}
 
 export default Header
