a2577bec8c609b97e96a8eec704fa1b1df95b2d4
jfw2855 <jwaddle1130@gmail.com>
2022-07-16 17:24:46 -0500

    renders a message if project does not have an issue

--- src/components/issues/IssueDetails.js
+++ src/components/issues/IssueDetails.js
@@ -7,6 +7,7 @@ const IssueDetails = (props) => {
     const {issues} = props
     const [sortState,setSortState] = useState("status")
     const [sort,setSort] = useState(false)
+    let items
 
     useEffect(()=> {
         sortIssues()
@@ -36,38 +37,48 @@ const IssueDetails = (props) => {
     
     sortIssues()
     //maps issues to create list group items of issues
-    let items = issues.map((issue)=>{
-        return(
-        <>
-        <Link
-            className="link-style"
-            to={`/issue/${issue._id}`}>
-            <ListGroupItem key={`issueKey${issue._id}`}>
-                <Row>
-                    <Col
-                    className="issue-details"
-                    style={{backgroundColor: issue.status==='open'?'lightgreen':'lightgrey'}}
-                    >
-                        {issue.status}
-                    </Col>
-                    <Col
-                    className="issue-details"
-                    style = {{
-                        'backgroundColor': issue.priority === 'low' ?
-                        'lightyellow' : issue.priority === 'medium' ?
-                        'yellow' : issue.priority === 'high'? 'orange':'red'
-                    }}
-                    >
-                        {issue.priority}
-                    </Col>
-                    <Col className="issue-details overflow">{issue.title}</Col>
-                    <Col className="issue-details">{issue.comments.length}</Col>
-                </Row>
-            </ListGroupItem>
-        </Link>
-        </>
-        )
-    })
+    if (issues.length>0) {
+        items = issues.map((issue)=>{
+            return(
+            <>
+            <Link
+                className="link-style"
+                to={`/issue/${issue._id}`}>
+                <ListGroupItem key={`issueKey${issue._id}`}>
+                    <Row>
+                        <Col
+                        className="issue-details"
+                        style={{backgroundColor: issue.status==='open'?'lightgreen':'lightgrey'}}
+                        >
+                            {issue.status}
+                        </Col>
+                        <Col
+                        className="issue-details"
+                        style = {{
+                            'backgroundColor': issue.priority === 'low' ?
+                            'lightyellow' : issue.priority === 'medium' ?
+                            'yellow' : issue.priority === 'high'? 'orange':'red'
+                        }}
+                        >
+                            {issue.priority}
+                        </Col>
+                        <Col className="issue-details overflow">{issue.title}</Col>
+                        <Col className="issue-details">{issue.comments.length}</Col>
+                    </Row>
+                </ListGroupItem>
+            </Link>
+            </>
+            )
+        })
+    }
+    else {
+        items =
+        <ListGroupItem>
+        <Row>
+            <i>No Issues Created For Project</i>
+        </Row>
+    </ListGroupItem>
+    }
 
 
 
