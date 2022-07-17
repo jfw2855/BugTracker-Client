becf8214d8579bbfd0f48f04a7ca07c0edd18be9
jfw2855 <jwaddle1130@gmail.com>
2022-07-17 18:17:51 -0500

    adds handle functions for deleting projects or issues

--- src/components/shared/DeleteModal.js
+++ src/components/shared/DeleteModal.js
@@ -1,23 +1,44 @@
 
-import {Modal, Container, Form, Button, Col, Row } from 'react-bootstrap'
+import {Button, Modal} from 'react-bootstrap'
+import { removeAllIssues, removeIssue } from '../../api/issue'
+import { removeProject } from '../../api/project'
+import { useNavigate } from 'react-router-dom'
 
 
 const DeleteModal = (props) => {
 
-    const { show, user, handleClose} = props
+    const { show, user, handleClose, projId, issueId, projectNavId} = props
+    const navigate = useNavigate()
 
+     // handle delete function that will remove project from db
+    const deleteProject = async(e) => {
+        e.preventDefault()
+        await removeAllIssues(user,projId)
+        await removeProject(user,projId)
+        //returns back to home page after removing project
+        navigate('/')
+    }
 
+    // handle delete function that will remove issue from db
+    const deleteIssue = async(e) => {
+        e.preventDefault()
+        await removeIssue(user,issueId)
+        //returns back to project show page after removing issue
+        navigate(`/project/${projectNavId}`)
+    }
+
+    // 
 
 	return (
         <Modal show={show} onHide={handleClose} className="modal">
             <Modal.Header closeButton>
-                Delete
+                Delete {issueId?"Issue":"Project"}
             </Modal.Header>
             <Modal.Body style={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-                <p>Click to Delete Project</p>
-                <button className='modal-btn'>
-                    Click Me
-                </button>
+                <p>Item cannot be retrieved once deleted</p>
+                <Button className='modal-btn' variant="danger" onClick={issueId?deleteIssue:deleteProject}>
+                    Delete {issueId?"Issue":"Project"}
+                </Button>
 
             </Modal.Body>
 
