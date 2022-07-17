58968bcff0611d75e0562acc22b631e7549182ed
jfw2855 <jwaddle1130@gmail.com>
2022-07-16 23:40:51 -0500

    adds modal component to file

--- src/components/shared/DeleteModal.js
+++ src/components/shared/DeleteModal.js
@@ -1,10 +1,27 @@
 
+import {Modal, Container, Form, Button, Col, Row } from 'react-bootstrap'
+
 
 const DeleteModal = (props) => {
+
+    const { show, user, handleClose} = props
 
 
 
 	return (
+        <Modal show={show} onHide={handleClose} className="modal">
+            <Modal.Header closeButton>
+                Delete
+            </Modal.Header>
+            <Modal.Body style={{display:'flex',alignItems:'center',flexDirection:'column'}}>
+                <p>Click to Delete Project</p>
+                <button className='modal-btn'>
+                    Click Me
+                </button>
+
+            </Modal.Body>
+
+        </Modal>
 	)
 }
 
